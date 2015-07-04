3aeaa58017e3e96fa62a1cee368e4f99539c96bb
Brian King <free.as.in.speech@gmail.com>
2015-07-04 15:40:29 +0200

    Use custom tableview cell layout

--- RoshamboWithHistory_incomplete/RockPaperScissors/HistoryViewController.swift
+++ RoshamboWithHistory_incomplete/RockPaperScissors/HistoryViewController.swift
@@ -11,6 +11,12 @@ import UIKit
 
 class HistoryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var history: [RPSMatch]!
+    let PLAYED_TAG = 1
+    let RESULT_TAG = 2
+    let VS_TAG = 3
+    let CONTAINER_TAG = 4
+    let winningBgColor = UIColor(red: CGFloat(0), green: CGFloat(0.9), blue: CGFloat(0.2), alpha: CGFloat(0.1))
+    let losingBGColor = UIColor(red: CGFloat(0.8), green: CGFloat(0.1), blue: CGFloat(0.1), alpha: CGFloat(0.1))
 
     @IBOutlet var tableView: UITableView!
     @IBOutlet weak var navigationBar: UINavigationBar!
@@ -21,6 +27,7 @@ class HistoryViewController: UIViewController, UITableViewDelegate, UITableViewD
     }
 
     override func viewWillAppear(animated: Bool) {
+        // Show last results first
         self.history = self.history.reverse()
     }
 
@@ -33,19 +40,34 @@ class HistoryViewController: UIViewController, UITableViewDelegate, UITableViewD
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let historyCell = self.tableView.dequeueReusableCellWithIdentifier("historyCell") as! UITableViewCell
+        let playedLabel = historyCell.viewWithTag(PLAYED_TAG) as! UILabel
+        let resultLabel = historyCell.viewWithTag(RESULT_TAG) as! UILabel
+        let versusLabel = historyCell.viewWithTag(VS_TAG) as! UILabel
         let match = self.history[indexPath.row]
-        var result: String
-        if match.p1 == match.p2 {
-            result = "Tie"
-        } else if match.p1 == match.winner {
-            result = "Won vs. \(match.p2)"
-        } else {
-            result = "Lost vs. \(match.p2)"
-        }
-        historyCell.textLabel?.text = "\(match.p1)"
-        historyCell.detailTextLabel?.text = result
-        return historyCell
 
+        playedLabel.text = "\(match.p1)"
+        if match.p1 == match.p2 {
+            resultLabel.text = "Tie"
+            versusLabel.text = ""
+        } else {
+            versusLabel.text = "vs. \(match.p2)"
+            if match.p1 == match.winner {
+                resultLabel.text = "Won"
+                historyCell.contentView.backgroundColor = self.winningBgColor
+            } else {
+                resultLabel.text = "Lost"
+                historyCell.contentView.backgroundColor = self.losingBGColor
+            }
+        }
+
+        return historyCell
+    }
+
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        // Adjust the cell width so that it matches the table's width:
+        cell.separatorInset = UIEdgeInsetsZero
+        cell.preservesSuperviewLayoutMargins = false
+        cell.layoutMargins = UIEdgeInsetsZero
     }
 
     @IBAction func dismiss(sender: UIBarButtonItem) {
